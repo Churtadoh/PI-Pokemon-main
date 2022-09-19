6e14c8140253726b3526043d1bb4923e32bba9fc
Camilo <churtadoh92@gmail.com>
2022-09-19 17:29:28 -0500

    update

--- api/src/models/Pokemon.js
+++ api/src/models/Pokemon.js
@@ -4,9 +4,30 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('pokemon', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false
+    },
     name: {
-      type: DataTypes.STRING,
+      type: DataTypes.STRING(20),
+      allowNull: false,
+      unique: true
+    },
+    height: {
+      type: DataTypes.INTEGER,
       allowNull: false,
     },
-  });
+    weight: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  },{timestamps: false});
+
+  sequelize.define('type',{
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  },{timestamps: false})
 };
