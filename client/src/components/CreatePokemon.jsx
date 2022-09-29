52c26e98a93d8b8535b5b422e33e4cfb4f9a4df2
Camilo <churtadoh92@gmail.com>
2022-09-28 22:40:44 -0500

    update

--- client/src/components/CreatePokemon.jsx
+++ client/src/components/CreatePokemon.jsx
@@ -1,8 +1,9 @@
 import { useState } from "react"
 import axios from "axios"
 import {useEffect} from "react"
-import { getTypes } from "../redux/actions"
+import { getAllPokemon, getTypes } from "../redux/actions"
 import { connect} from "react-redux"
+import s from './CreatePokemon.module.css'
 
 const CreatePokemon = (props) =>{
     useEffect(()=> {
@@ -11,12 +12,11 @@ const CreatePokemon = (props) =>{
 
       const arrayTypes = props.types.map(el =>{
         return(<div key={el.id}>
-            id: {el.id}
-            name: {el.name}
+            {`${el.id} : ${el.name}`}
             </div>)
-      })  
-
-    const[input,setInput] = useState({
+      })
+    
+    const initialInput =  {
         name: '',
         id: '',
         height: '',
@@ -27,41 +27,57 @@ const CreatePokemon = (props) =>{
         speed:'',
         types:'',
         img:''
-    })
+    } 
+
+    const[input,setInput] = useState(initialInput)
+
+    const[errors,setErrors] = useState({})
 
     function handleInputChange(e) {
-        setInput({
+        const newInput = {
             ...input,
             [e.target.name] : e.target.value
-        })
+        }
+        setInput(newInput)
+        setErrors(validate(newInput))
+    }
+
+    function validate(input) {
+        let errors = {};
+        if(!input.id)  {
+            errors.id = "Id is required"
+        } else if(!/^U+(?=.*[0-9])/.test(input.id)) {
+            errors.id = "Id must start with U followed by a number"
+        }
+        if(!input.types) {
+            errors.types = "Type is required"
+        } else if(!/(?=.*[0-9])/.test(input.types)){
+            errors.types = "Types must be a number or series of numbers"
+        }
+        return errors
     }
 
     const handleSubmit = (e) =>{
         e.preventDefault();
         axios.post("http://localhost:3001/pokemon", input)
         .then(res => {console.log(res)
-        setInput({
-            name: '',
-            id: '',
-            height: '',
-            weight: '',
-            hp: '',
-            attack:'',
-            defense:'',
-            speed:'',
-            types:'',
-            img:''
-        })
+        setInput(initialInput)
+        props.getAllPokemon()
         })
     }
 
 
     return(
-       <form onSubmit={handleSubmit}>
+        <div>
+        <h1 className={s.create}>Create your own pokemon</h1>
+        <div className={s.general}>   
+       <form className={s.form} onSubmit={handleSubmit}>
           <label>Name: </label>
           <input name = 'name' value = {input.name} onChange = {handleInputChange}/><br/>
           <label>Id: </label>
-          <input name = 'id' value = {input.id} onChange = {handleInputChange}/><br/>
+          <input name = 'id' value = {input.id} onChange = {handleInputChange} 
+          className ={errors.id ? s.danger : ""}/><br/>
+          {errors.id && <span className={s.danger}>{errors.id}</span>}
           <label>Height: </label>
           <input name = 'height' value = {input.height} onChange = {handleInputChange}/><br/>
           <label>Weight: </label>
@@ -77,17 +93,27 @@ const CreatePokemon = (props) =>{
           <label>Image URL: </label>
           <input name = 'img' value = {input.img} onChange = {handleInputChange}/><br/>
           <label>Types: </label>
-          <input name = 'types' value = {input.types} onChange = {handleInputChange}/><br/>
-    
-
-          <button type="submit">Create</button>
-
-          <div>
-            <h3>Possible Types</h3>
-            <div>{arrayTypes}</div>
-          </div>
-          
+          <input name = 'types' value = {input.types} onChange = {handleInputChange}
+          className ={errors.id ? s.danger : ""}
+          /><br/>
+          {errors.types && <span className={s.danger}>{errors.types}</span>}
+          <button className={s.button} disabled={errors.id? "disabled": ""} type="submit">Create</button>
        </form> 
+          <div className={s.types}>
+            <h3>Possible Types</h3>
+            <div className={s.types}>{arrayTypes}</div>
+            <div className={s.notice}>
+                <h2>Notice!</h2>
+                <p className={s.p}>To choose the type of your pokemon 
+                    type the id into the input box, if
+                    you wish to choose more than one type
+                    insert the different Id's separated by
+                    commas. e.g., ("1,15" for normal and ice)
+                </p>    
+            </div>
+          </div>
+       </div>
+       </div>
     )
 }
 
@@ -99,7 +125,8 @@ export const mapStateToProps = (state) => {
 
 export const mapDispatchToProps = (dispatch) => {
     return{
-        getTypes: () => dispatch(getTypes())
+        getTypes: () => dispatch(getTypes()),
+        getAllPokemon: () => dispatch(getAllPokemon())
     }
   }
   
