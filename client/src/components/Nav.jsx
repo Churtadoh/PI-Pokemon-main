d035401197972df1f2537011cb501d56ef718818
Camilo <churtadoh92@gmail.com>
2022-10-02 01:16:26 -0500

    update

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,17 +1,27 @@
 import {Link} from 'react-router-dom'
 import {useEffect} from 'react';
-import { getPokemonQuery } from '../redux/actions';
+import { getPokemonQuery, getPokemon } from '../redux/actions';
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import s from './Nav.module.css'
 
-const Nav = (props) =>{
+const Nav = () =>{
 
+let initalRandom = Math.floor((Math.random()*500))    
+
+const [random , setRandom] = useState(initalRandom)    
 const [input,setInput] = useState('')
 const [inputSearch, setInputSearch] = useState('')
 
+const handleRandom = () => {
+    let a = Math.floor((Math.random()*500))
+    setRandom(a)
+    dispatch(getPokemon(random))
+}
+
 const handleInputSearch = () =>{
     setInputSearch(input)
+    setInput('')
 }
 
 const dispatch = useDispatch()
@@ -30,6 +40,7 @@ return(
         <div>
            <input id='input' className={s.bar} value={input} onChange= {handleInputChange} placeholder='Search for pokemon by name'/>
            <Link to = {`/pokemon?name=${input}`}><button type='submit' className={s.button} onClick= {handleInputSearch}>Search</button></Link>
+           <Link to = {`/pokemon/${random}`}><button onClick={handleRandom} className={s.button1}>Random</button></Link>
         </div>
     </div> 
 )
--- client/src/components/PokemonDetail.jsx
+++ client/src/components/PokemonDetail.jsx
@@ -6,19 +6,28 @@ const PokemonDetail = () =>{
     const pokemon = useSelector(state => state.pokemon)
 
     return(
-        <div>
-        <h1 className={s.name}>Name: {pokemon.name} </h1> 
-        <img src={pokemon.img} alt=''/>
-        <h3>Id: {pokemon.id}</h3>
-        <h3>Type(s): {pokemon.types}</h3>
-        <span><h3>Height: {pokemon.height} Weight: {pokemon.weight}lbs</h3></span>
-        <ul>Stats:
+        <div >
+          <h1 className={s.name}>Name: {pokemon.name} </h1>
+          <div className={s.general}>
+          <div> 
+            <img src={pokemon.img} alt='' className={s.img}/>
+          </div>
+          <div >
+            <h3>Id: {pokemon.id}</h3>
+            <h3>Type(s): {pokemon.types}</h3>
+            <span><h3>Height: {pokemon.height} Weight: {pokemon.weight}lbs</h3></span>
+          </div>
+
+          <div className={s.stats}>
+          <h4 className={s.h3}>STATS:</h4>  
+          <ul className={s.ul}>
             <li>HP: {pokemon.hp}</li>
             <li>ATTACK: {pokemon.attack}</li>
             <li>DEFENSE: {pokemon.defense}</li>
             <li>SPEED: {pokemon.speed}</li>
-        </ul>
-
+          </ul>
+          </div>
+          </div>
         </div>
     )
 }
